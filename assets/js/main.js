85298d498710f9fdd8ef88f7752e307ca19e1092
KodakPrism611 <kokoulin987@gmail.com>
2024-09-03 15:52:16 +0300

    pr1

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let name = prompt('Введите ваше имя', 'Толик');
+alert(`ваше имя ${name}`);
+
+let surname = prompt('Введите вашу фамилию', 'Толкович');
+alert(`вашу фамилию ${surname}`);
+
+let fathername = prompt('Введите ваше отчество', 'Толикович');
+alert(`ваше отчество ${fathername}`);
+
+let isTrue =  confirm('Верны ли ваши данные?');
+alert(isTrue);
